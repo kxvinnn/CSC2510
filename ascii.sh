ac8ac731812a42ce41638b72197ab26381dc8669
kxvinnn <kevdamiang14@gmail.com>
2024-10-28 03:26:56 +0000

    ascii christmas tree

---
+++ ascii.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 	"      *"
+echo -e "\e[32m     / \\"
+echo -e "\e[32m    /   \\"
+echo -e "\e[32m   /_____\\"
+echo -e "\e[33m      ||"
+echo -e "\e[33m      ||"
+echo -e "\e[0m"
+
